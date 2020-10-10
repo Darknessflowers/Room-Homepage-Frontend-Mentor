77a605b0e194824aced15689b3db7c57832b8986
Darknessflowers <darknessflowers@hotmail.co.uk>
2020-10-10 16:41:45 +1300

    Tidy js

--- scripts.js
+++ scripts.js
@@ -1,26 +1,3 @@
-// let slides = [
-// {
-//   title: "Discover innovative ways to decorate",
-//   paragraph: "We provide unmatched quality, comfort, and style for property owners across the country.Our experts combine form and function in bringing your vision to life. Create a room in your own style with our collection and make your property a reflection of you and what you love.",
-//   image: "./images/desktop-image-hero-1.jpg",
-//   mobileImage: "./images/mobile-image-hero-1.jpg",
-//   ctaText: "Shop now",
-// },
-// {
-//   title: "We are available all across the globe",
-//   paragraph: "With stores all over the world, it's easy for you to find furniture for your home or place of business.Locally, we’re in most major cities throughout the country. Find the branch nearest you using our store locator. Any questions? Don't hesitate to contact us today.",
-//   image: "./images/desktop-image-hero-2.jpg",
-//   mobileImage: "./images/mobile-image-hero-2.jpg",
-//   ctaText: "Shop now",
-// },
-// {
-//   title: "Manufactured with the best materials",
-//   paragraph: "Our modern furniture store provide a high level of quality. Our company has invested in advanced technology to ensure that every product is made as perfect and as consistent as possible. With three decades of experience in this industry, we understand what customers want for their home and office.",
-//   image: "./images/desktop-image-hero-3.jpg",
-//   mobileImage: "./images/mobile-image-hero-3.jpg",
-//   ctaText: "Shop now",
-// },
-// ];
 let slider = document.querySelector('#slider');
 let slides = document.querySelectorAll('.slide');
 let images = document.querySelectorAll('#imageContainer img');
@@ -32,11 +9,6 @@ const controls = document.querySelector('#controls');
 let menuBtn = document.querySelector('.openMenu');
 let header = document.querySelector('header');
 let switchImgBreakpoint = window.matchMedia('(max-width: 800px)');
-
-// let sliderImg = document.querySelector('#sliderImg img');
-// const prevSlideArrow = document.querySelector('#prevSlide');
-// const nextSlideArrow = document.querySelector('#nextSlide');
-
 
 function nextSlide() {
   if(currentSlide < images.length - 1) {
@@ -71,7 +43,6 @@ function autoplaySlider() {
   }, 6000);
 }
 function pauseSliderAutoplay() {
-  console.log('pausing');
   clearInterval(autoplay);
   slider.addEventListener('mouseout', autoplaySlider, true);
 }
